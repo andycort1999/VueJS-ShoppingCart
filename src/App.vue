12955a1dfc2b1da35c37a1fc9c8841e9ec011dec
andycort1999 <119467957+andycort1999@users.noreply.github.com>
2023-10-02 18:50:52 +0000

    Facture enlazado con checkboxes

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,19 @@ const items = ref([
     {id: 2, label: '1 lata de frijoles'},
     {id: 3, label: '2 lata de atún'},
 ]);
-
+const newitem=ref('1 bolsa de te');
+const newitemhighpriority=ref(false);
 </script>
 
 <template>
   <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+  <input v-model.trim="newitem" placeholder="Ingresar asrticulo">
+  <!-- check boxes -->
+<leabel>
+  <input v-model="newitemhighpriority" type="checkbox">
+  Alta preoridad
+</leabel>
+  {{ newitemhighpriority ? "fuego" : "hielo"}}
   <ul>
     <li v-for="{ id, label } in items" v-bind:key="id">🔹 {{label}}</li>
   </ul>
